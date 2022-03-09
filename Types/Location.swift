c2b01ddc33523475b60309644bd8627d8f58e7dd
David <davidsant409@csu.fullerton.edu>
2022-03-09 08:09:40 -0800

    location, food location, amenity location types added test added for new types

---
+++ Types/Location.swift
@@ -0,0 +1,23 @@
+//
+//  Location.swift
+//  CSUF_Explorer
+//
+//  Created by David Santana on 3/8/22.
+//
+
+import Foundation
+
+class Location {
+    var lat: Double
+    var long: Double
+    var name: String
+    var description: String?
+    var hours: [String: String]?
+    var type: String?
+
+    init(lat: Double, long: Double, name: String) {
+        self.lat = lat
+        self.long = long
+        self.name = name
+    }
+}

7570b130c0557a0cccb97f2abf2dbec80a036265
Minh Nguyen <minhnguyenandrew@csu.fullerton.edu>
2022-03-22 08:05:23 -0700

    Working map + pins of some places at CSUF

--- CSUF_Explorer/ViewController.swift
+++ CSUF_Explorer/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-//        @IBOutlet private var mapView: MKMapView!
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }

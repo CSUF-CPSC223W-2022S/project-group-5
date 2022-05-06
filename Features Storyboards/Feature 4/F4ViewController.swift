ff7c653dc96a1275a9d29d04a9471ceae81919ae
Andy Garcia <andygarciapc@gmail.com>
2022-05-05 23:34:46 -0700

    Added Colors/Feature 4 outline

--- Features Storyboards/Feature 4/F4ViewController.swift
+++ Features Storyboards/Feature 4/F4ViewController.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 class F4ViewController: UIViewController {
+    
+    
+    @IBOutlet var nearbytableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nearbytableView.delegate = self
+        nearbytableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -23,4 +30,21 @@ class F4ViewController: UIViewController {
          // Pass the selected object to the new view controller.
      }
      */
+}
+
+extension F4ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let nearbyCell = nearbytableView.dequeueReusableCell(withIdentifier: "nearbyViewCell", for: indexPath)
+        nearbyCell.textLabel?.text = "Test Cell"
+        return nearbyCell
+    }
+    
 }

99adf1493f5114f2b43aaf1823c9b05dcb90f738
Andy Garcia <andygarciapc@gmail.com>
2022-05-11 22:07:09 -0700

    Update class/ add cells

--- Types/NearbyLocation.swift
+++ Types/NearbyLocation.swift
@@ -8,7 +8,40 @@
 import Foundation
 
 class NearbyLocation : Location {
+    var todaysHours : String {
+        let temp = LocationCard()
+        
+        let today = temp.getDayOfWeek() ?? ""
+        
+        return self.hours[today] ?? "\(today) Hours"
+    }
+    
     override init(lat: Double, long: Double, name: String) {
         super.init(lat: lat, long: long, name: name)
+        initDays()
+    }
+    
+    init(lat: Double, long: Double, name: String, location : String)
+    {
+        super.init(lat: lat, long: long, name: name)
+        self.address = location
+        initDays()
+    }
+    
+    func initDays()
+    {
+        self.hours["Tuesday"] = "Hours"
+        self.hours["Wednesday"] = "Hours"
+        self.hours["Thursday"] = "Hours"
+        self.hours["Friday"] = "Hours"
+        self.hours["Saturday"] = "Hours"
+        self.hours["Sunday"] = "Hours"
+    }
+    
+    func setAllHours(hour: String)
+    {
+        for (day, _) in self.hours{
+            self.hours[day] = hour
+        }
     }
 }
